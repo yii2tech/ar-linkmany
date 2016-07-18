6673220b6c2ae54d2cda46037131a387906e39e5
sokoji <kirill.menkov@gmail.com>
2016-07-18 11:41:13 +0300

    #7 — saving without relation field

--- LinkManyBehavior.php
+++ LinkManyBehavior.php
@@ -61,12 +61,14 @@ class LinkManyBehavior extends Behavior
     /**
      * @var array additional column values to be saved into the junction table.
      * Each column value can be a callable, which will be invoked during linking to compose actual value.
+     * Starting from version 1.0.2, this callable may accept linked model instance as a first parameter.
      * For example:
      *
      * ```php
      * [
      *     'type' => 'user-defined',
      *     'createdAt' => function() {return time();},
+     *     'categoryId' => function ($model) {return $model->categoryId;},
      * ]
      * ```
      */
@@ -262,7 +264,7 @@ class LinkManyBehavior extends Behavior
         }
 
         foreach ($linkModels as $model) {
-            $this->owner->link($this->relation, $model, $this->composeLinkExtraColumns());
+            $this->owner->link($this->relation, $model, $this->composeLinkExtraColumns($model));
         }
     }
 
@@ -277,9 +279,10 @@ class LinkManyBehavior extends Behavior
 
     /**
      * Composes actual link extra columns value from [[extraColumns]], resolving possible callbacks.
+     * @param ActiveRecordInterface|null $model linked model instance.
      * @return array additional column values to be saved into the junction table.
      */
-    protected function composeLinkExtraColumns()
+    protected function composeLinkExtraColumns($model = null)
     {
         if (empty($this->extraColumns)) {
             return [];
@@ -287,7 +290,7 @@ class LinkManyBehavior extends Behavior
         $extraColumns = [];
         foreach ($this->extraColumns as $column => $value) {
             if (!is_scalar($value) && is_callable($value)) {
-                $value = call_user_func($value);
+                $value = call_user_func($value, $model);
             }
             $extraColumns[$column] = $value;
         }
